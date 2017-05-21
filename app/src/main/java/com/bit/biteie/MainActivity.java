7a633b559e8c704d1ded4afa17e0ee036649b658
Syed Misbah Ahmed <misbah.ahmed.0001@gmail.com>
2017-05-21 15:22:06 +0530

    Nav drawer added

--- app/src/main/java/com/bit/biteie/MainActivity.java
+++ app/src/main/java/com/bit/biteie/MainActivity.java
@@ -2,8 +2,15 @@ package com.bit.biteie;
 
 import android.content.Intent;
 import android.os.Build;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -11,7 +18,8 @@ import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
 
 
 
@@ -31,6 +39,18 @@ public class MainActivity extends AppCompatActivity {
 
 
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
 
 
 
@@ -43,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
         final Button BtnNotes = (Button)findViewById(R.id.BtnNotes);
         final Button BtnFaculty = (Button)findViewById(R.id.BtnFaculty);
         final Button BtnCirculars = (Button)findViewById(R.id.BtnCircular);
-        final ImageButton info = (ImageButton)findViewById(R.id.BtnInfo);
+        //final ImageButton info = (ImageButton)findViewById(R.id.BtnInfo);
 
 
         final AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.5F);
@@ -133,16 +153,68 @@ public class MainActivity extends AppCompatActivity {
         }
         );
 
-        info.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-            v.startAnimation(buttonClick);
-            Intent intent10 = new Intent(MainActivity.this, InfoActivity.class);
-            startActivity(intent10);}
-                                        }
-        );
 
- }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+   /* @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    */
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_about_us) {
+
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_credits) {
+
+        } else if (id == R.id.nav_support) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_feedback) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
 
 
